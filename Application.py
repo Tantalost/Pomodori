704bc5be2e586e7dbe4e76698c5577cb05811446
Lol <133163245+Tantalost@users.noreply.github.com>
2025-05-16 17:55:46 +0800

    making the application

--- Application.py
+++ Application.py
@@ -3,7 +3,9 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Pomodori")
 
-label = tk.Label(window, text="c")
+label = tk.Label(window, text="This will make the application")
 label.pack()
+
+window.geometry("1920x1080")
 
 window.mainloop()
