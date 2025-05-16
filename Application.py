45acc9c2355fc8002c4c4151e6b6b9191c5955ef
Lol <133163245+Tantalost@users.noreply.github.com>
2025-05-16 18:24:18 +0800

    changes for today

--- Application.py
+++ Application.py
@@ -1,12 +1,30 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+window = Tk()
 window.title("Pomodori")
 
-label = tk.Label(window, text="This will make the application")
+label = Label(window, text="This will make the application")
 label.pack()
 
 window.geometry("1920x1080")
 background = PhotoImage(file = "bg.gif")
+
+label1 = Label( window, image = background)
+label1.place(x = 0, y = 0)
+
+label2 = Label( window, text = "Welcome")
+label2.pack(pady = 50)
+
+frame1 = Frame(window)
+frame1.pack(pady = 20 )
+
+button1 = Button(frame1,text="Exit")
+button1.pack(pady=20)
+
+button2 = Button( frame1, text = "Start")
+button2.pack(pady = 20)
+
+button3 = Button( frame1, text = "Reset")
+button3.pack(pady = 20)
 
 window.mainloop()
